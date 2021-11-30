d9a11fc54cd8a2df2c64668f2f3ac5383ed7393b
ThanhVinh <yoonhee6668@gmail.com>
2021-11-30 17:12:34 +0700

    fix chart.js responsive

---
+++ chart1.js
@@ -0,0 +1,22 @@
+const ctx = document.getElementById('lineChart').getContext('2d');
+const myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['Tháng 1','Tháng 2','Tháng 3','Tháng 4','Tháng 5','Tháng 6','Tháng 7','Tháng 8','Tháng 9','Tháng 10','Tháng 11','Tháng 12'],
+        datasets: [{
+            label: 'Doanh thu (triệu VND)',
+            data: [12, 19, 3, 5, 2, 3,10000,40000,1000, 3000,2111,123],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)'
+                
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true
+    }
+});
